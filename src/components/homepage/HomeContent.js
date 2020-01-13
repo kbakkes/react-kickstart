c6a2904147d4877d711a6f38b9d06d04dd2e5340
kbakkes <karim.bakkes@gmail.com>
2020-01-13 20:23:40 +0100

    various styling

--- src/components/homepage/HomeContent.js
+++ src/components/homepage/HomeContent.js
@@ -4,19 +4,19 @@ import React from 'react';
  function HomeContent()  {
 
         return(
-            <div class="home-content">
-                <div class="home-content__tagline">
-                    <p class="content">Tagline content about the company goes around here.</p>
-                </div>
-                <div class="home-content__intro">
-                    Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen.
-                    Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
-                    toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde
-                    om een font-catalogus te maken.
-                    Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd,
-                    overgenomen in elektronische letterzetting.
-                </div>
 
+            <div class="home-content__wrapper">
+                <div class="home-content">
+                    <div class="home-content__tagline">
+                        <p class="content">Tagline content about the company goes around here.</p>
+                    </div>
+                    <div class="home-content__intro">
+                        Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen.
+                        Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw,
+                        toen een onbekende drukker een zethaak met.
+                    </div>
+                </div>
+                <a href="#" className="action-primary">Lees meer</a>
             </div>
         )
 
